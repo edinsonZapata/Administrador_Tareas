c0dba4cabe8b89ca63500724f0e5f8b8d22a1d97
sebastianPabon <pabonlopezsebastian@gmail.com>
2022-07-26 10:30:13 -0500

    AT-0001-DP: Creación componente login
    + Se crea el componente de login

---
+++ src/app/components/login-view/LoginVue.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container border rounded border-secondary mt-5 container-view-login">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-black">titulo</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script src="./loginVue.ts" lang="ts"></script>
+<style scoped lang="scss">
+@import "./login-view.scss";
+</style>
