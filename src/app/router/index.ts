d08e934010d8c86e9802aea586e37e68d16063d1
andreselduro <104477633+andreselduro@users.noreply.github.com>
2022-08-01 19:50:53 -1100

    agg record

---
+++ src/app/router/index.ts
@@ -0,0 +1,47 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+
+const AppLogin = () => import("../components/login-view/LoginVue.vue");
+
+const AppRegister = () => import("../components/register-view/RegisterVue.vue");
+
+const AdminMain = () => import("../components/home-page/HomePage.vue");
+
+const Adminrecord = () => import("../components/record-page/recordPage.vue");
+
+Vue.use(VueRouter);
+
+const routes = [
+{
+    path:"/login",
+    name:"login",
+    component: AppLogin
+},
+{
+    path:"/",
+    name:"index",
+    redirect:{name:"login"}
+
+},
+{
+    path:"/register",
+    name:"register",
+    component:AppRegister
+},
+{
+    path:"/app",
+    name:"admin",
+    component:AdminMain
+},
+{
+    path:"/record",
+    name:"admin",
+    component:Adminrecord
+}
+] 
+
+
+const router = new VueRouter({mode: "history", routes});
+
+export default router;
