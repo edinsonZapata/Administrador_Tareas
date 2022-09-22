1f4cc66aac5c8d5f650b7b1098d5bc3d3e4e922e
andreselduro <104477633+andreselduro@users.noreply.github.com>
2022-09-21 22:53:05 -0500

    Revert "Merge branch 'AT-0001-AP' into AT-0001-DP"
    This reverts commit 1b11990cf9a02cb80b9b75d2626176aa98f5e7b4, reversing
    changes made to 8df2189de36c2473395e9ff9379cc8930aa775d7.

--- src/app/components/record-Page/recordPage.ts
+++ src/app/components/record-Page/recordPage.ts
@@ -1,35 +1,9 @@
 import { Component, Prop } from "vue-property-decorator";
 import { VueWizard } from "@/vue-wizard";
-import { store, storeTypes } from "@/app/store";
 import $ from 'jquery';
-import axios from "axios";
-import { BASE_URL_MANAGER } from "@/config";
+
 @Component({
-    name: 'record-page',
+    name: 'record-Page',
 })
 
-export default class RecordPage extends VueWizard {
-    public openModalRefreshRecord: boolean = false;
-    public openModalCloseSesion: boolean = false;
-    showModalWindowCloseSesion(){
-        this.openModalCloseSesion = !this.openModalCloseSesion;
-    }
-
-    showModalWindowRefreshRecord(){
-        console.log("prueba")
-        this.openModalRefreshRecord = !this.openModalRefreshRecord;
-    }
-   
-    public records: any[] = []
-
-    async mounted(): Promise<any> {
-        await axios.get(`${BASE_URL_MANAGER}/tasks/consultAllRecord`);
-
-        // await store.dispatch(storeTypes.record.actions.getAllRecords())
-        // .then((records: any) => {
-        //     if(store.state.record){
-        //         this.records = store.state.record?.records;
-        //     }
-        // })
-    }
-}
+export default class recordPage extends VueWizard {}
